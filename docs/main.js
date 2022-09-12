c4035b33b1070d17ee5da05c98c7da6c8aa7a5e3
Said Valencia Castrillo <50384031+said05@users.noreply.github.com>
2022-09-12 16:25:47 -0500

    modified the names of app and style to main

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,9 @@
 const inputCard = document.querySelector('#input-card');
 const inputDate = document.querySelector('#input-date');
 const inputCVV = document.querySelector('#input-cvv');
-
 const maskNumber = '####-####-####-####';
 const maskDate = '##/##';
 const maskCVV = '###';
-
 let current = '';
 let cardNumber = [];
 let dateNumber = [];
@@ -40,12 +38,10 @@ inputCVV.addEventListener('keydown', (e) => {
 
 function handleInput(mask, key, arr) {
   let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
   if (key === 'Backspace' && arr.length > 0) {
     arr.pop();
     return;
   }
-
   if (numbers.includes(key) && arr.length + 1 <= mask.length) {
     if (mask[arr.length] === '-' || mask[arr.length] === '/') {
       arr.push(mask[arr.length], key);
